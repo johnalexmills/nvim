46b8db79c0f7ed91aca074e2b4aa4094677f9c04
johnalexmills <johnalexmills@icloud.com>
2023-08-27 19:40:18 -0400

    adding relative number default

--- lua/options.lua
+++ lua/options.lua
@@ -42,6 +42,7 @@ vim.opt.iskeyword:append "-"                    -- treats words with `-` as sing
 vim.opt.formatoptions:remove { "c", "r", "o" }  -- This is a sequence of letters which describes how automatic formatting is to be done
 vim.opt.linebreak = true
 vim.opt.colorcolumn = "80"
+vim.opt.relativenumber = true
 if package.config:sub(1,1) == '\\' then
   vim.cmd(':set shell=cmd.exe')
 end
